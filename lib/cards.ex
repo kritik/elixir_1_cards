1c232b2775aa657cefd33afc65adfe0bb3548972
Vladimir Krylov <vladimir.krylov@perfectline.co>
2019-12-02 21:37:51 +0200

    section4

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,20 +1,16 @@
 # to run console run iex -S mix
 # to install deps run mix deps.get
+# to compile help run mix docs
+# to test run mix test
 # in IEx run recompile to recompile all code
 # elixir immutability. Send to f-tion with modifing copying data
 defmodule Cards do
   @moduledoc """
-  Documentation for Cards.
+  Provides methods for creating and handling a deck of cards.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
+  Returns a list of strings representing a deck of playing cards.
   """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
@@ -34,12 +30,31 @@ defmodule Cards do
     deck|>Enum.shuffle
   end
   
+  @doc """
+    Determines whether a deck contains a given card
+    
+  ## Examples
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck,"Two of Spades")
+      true
+  """
   def contains?(deck, card) do
     deck|>Enum.member?(card)
   end
   
   # Cards.deal(deck, 5) # { *hand*, *deck* }
   # Cards.deal(deck, 5) # { hand: [], deck: [] } - in ruby
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should
+    be in the hand.
+    
+    ## Examples
+        iex> deck = Cards.create_deck
+        iex> {hand, deck} = Cards.deal(deck,2)
+        iex> hand
+        ["Ace of Spades", "Two of Spades"]
+  """
   def deal(deck, hand_size) do
     deck|>Enum.split(hand_size)
   end
--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,14 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "create_desk creates 20 cards" do
+    assert length(Cards.create_deck) == 20
+  end
+  
+  test "shuffeling a deck randomizes it" do
+    deck = Cards.create_deck
+    new_deck = Cards.shuffle(deck)
+    assert length(deck) == length(new_deck)
+    refute deck == new_deck
   end
 end
